b94376b25d292a19cc5810a71770e116e86d0122
Bernd Artmüller <berndartmueller@gmail.com>
2020-09-01 16:05:23 +0200

    docs(website): add og_image and twitter image

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -67,8 +67,8 @@ const siteConfig = {
   cleanUrl: true,
 
   // Open Graph and Twitter card images.
-  ogImage: 'img/undraw_online.svg',
-  twitterImage: 'img/undraw_tweetstorm.svg',
+  ogImage: 'img/virchual.jpg',
+  twitterImage: 'img/virchual.jpg',
 
   // For sites with a sizable amount of content, set collapsible to true.
   // Expand/collapse the links and subcategories under categories.
