b33d0ad1e9b545b7c7a696d21a302040fdaf3fd0
Bernd Artmüller <berndartmueller@gmail.com>
2020-09-02 17:00:20 +0200

    Deploy website
    Deploy website version based on 7ad358ce95e07c97811f0ed2823c2c8c66e5caed

--- js/index.js
+++ js/index.js
@@ -1,29 +1,35 @@
-console.log('test');
-
 document.addEventListener(
   'DOMContentLoaded',
   function () {
     const instance = new Virchual(document.querySelector('.image-swiper'), {
       slides: function () {
+        const unsplashImageIds = [
+          '7EsFOzUoIzU',
+          'lUtPqjz5D5k',
+          '1-9nfcJ7-Zk',
+          'ZmQh4ojSA-k',
+          '9ZaqDVDdMwg',
+          'OhqiMvM6NsE',
+          'C8mZsNgGZk0',
+          'am6_dPRmWwI',
+          'M2FEVCu4Osw',
+        ];
         const slides = [];
 
         // add 9 more slides to a total of 10
         for (let i = 1; i < 10; i++) {
+          const imageId = unsplashImageIds[i - 1];
+
           slides.push(`
             <picture>
               <source
-                type="image/webp"
-                srcset="
-                  https://i.findheim.at/images/sm/6iBu4sycxr9kXMlcbyVyz.webp,
-                  https://i.findheim.at/images/md/6iBu4sycxr9kXMlcbyVyz.webp 2x
-                " />
-              <source
                 type="image/jpeg"
-                srcset="
-                  https://i.findheim.at/images/sm/6iBu4sycxr9kXMlcbyVyz.jpg,
-                  https://i.findheim.at/images/md/6iBu4sycxr9kXMlcbyVyz.jpg 2x
-                " />
-              <img src="https://i.findheim.at/images/md/6iBu4sycxr9kXMlcbyVyz.jpg" itemprop="image"/>
+                srcSet="
+                  https://source.unsplash.com/${imageId}/400x265,
+                  https://source.unsplash.com/${imageId}/800x530 2x
+                "
+              />
+              <img src="https://source.unsplash.com/${imageId}/400x265" itemProp="image" />
             </picture>
           `);
         }
@@ -32,10 +38,7 @@ document.addEventListener(
       },
     });
 
-    // instance.register(Controls, { isEnabled: true });
-
     instance.mount();
-    console.log('instance', instance);
   },
   false,
 );
