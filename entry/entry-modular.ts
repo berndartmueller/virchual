6f3d71adc78e67862fc66b3bdd89e0e09bd4dcb5
Bernd Artmüller <berndartmueller@gmail.com>
2020-09-03 23:19:08 +0200

    improve(entry): export Virchual also as non-default

--- entry/entry-modular.ts
+++ entry/entry-modular.ts
@@ -4,6 +4,8 @@ export { Controls } from './../src/components/controls/controls';
 export { Lazy } from './../src/components/lazy/lazy';
 export { LazyLoadImage } from './../src/components/lazyload-image/lazyload-image';
 
+export { Virchual };
+
 export default {
   Virchual,
 };
